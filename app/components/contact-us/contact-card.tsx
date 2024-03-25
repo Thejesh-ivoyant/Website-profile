017809b9fc394ca97e38170ae36cef1e73b83447
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-03-25 14:55:11 +0530

    conatct card update

--- app/components/contact-us/contact-card.tsx
+++ app/components/contact-us/contact-card.tsx
@@ -33,8 +33,8 @@ const ContactCard = () => {
             <div className="flex sm:gap-6 gap-2">
                 <img className="sm:w-28 w-20 aspect-square object-cover object-top" src={avatar}></img>
                 <div className="grid capitalize font-light">
-                    <small className="font-poppins text-xs">Office representative</small>
-                    <h1 className="font-medium font-poppins text-sm">Representative name</h1>
+                    <small className="font-poppins text-xs">Vice President - Commercial</small>
+                    <h1 className="font-medium font-poppins text-sm">Villa Elango</h1>
                     <>
                     <div className="flex font-normal leading-7 items-center gap-2 normal-case sm:text-base text-xs">
                         <svg className="sm:w-5 w-3 aspect-square"  viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.725 9.751a6.59 6.59 0 0 0 3.05 3.038.619.619 0 0 0 .612-.047l1.956-1.304a.625.625 0 0 1 .593-.055l3.66 1.569a.623.623 0 0 1 .374.648 3.75 3.75 0 0 1-3.72 3.275A10.625 10.625 0 0 1 3.625 6.25 3.75 3.75 0 0 1 6.9 2.53a.623.623 0 0 1 .648.375l1.57 3.662a.625.625 0 0 1-.052.589l-1.3 1.985a.62.62 0 0 0-.041.61v0Z" stroke="#969AFB" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
