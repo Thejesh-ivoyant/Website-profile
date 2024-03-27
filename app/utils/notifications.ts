9fe01cc20211dde44535e746cd298327629b7935
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-03-27 15:02:11 +0530

    notification

--- app/utils/notifications.ts
+++ app/utils/notifications.ts
@@ -9,20 +9,22 @@ type OpenMessageType = {
 };
 
 const withDefaultStyle = (WrappedMessage:any) => (options:any) => {
-  const defaultStyle = { marginTop: '20vh' }; // Define your default style here
+  const defaultStyle = { marginTop: '4.5rem' }; // Define your default style here
   return WrappedMessage({ ...options, style: { ...defaultStyle, ...options.style } }); // Merge styles
 };
 
 const openMessage = withDefaultStyle(message.success); // Wrap with success for default style
+const openMessageError = withDefaultStyle(message.error); // Wrap with success for default style
+const openMessageWarning = withDefaultStyle(message.warning); // Wrap with success for default style
 
 export const success = (content: React.ReactNode, duration?: number) => {
   openMessage({ type: 'success', content, duration });
 };
 
-export const errorMessage = (content: React.ReactNode, duration?: number, style?: React.CSSProperties) => {
-  openMessage({ type: 'error', content, duration, style });
+export const errorMessage = (content: React.ReactNode, duration?: number) => {
+  openMessageError({ type: 'error', content, duration});
 };
 
-export const warning = (content: React.ReactNode, duration?: number, style?: React.CSSProperties) => {
-  openMessage({ type: 'warning', content, duration, style });
+export const warning = (content: React.ReactNode, duration?: number) => {
+  openMessageWarning({ type: 'warning', content, duration });
 };
