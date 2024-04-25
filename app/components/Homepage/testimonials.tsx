4a486db8c3adef048a7a2e048446c78fa9dc7bc3
Bhavish Poojary <BhavishP@ivoyant.com>
2024-04-25 08:27:20 +0000

    Merged PR 306: Anchor placing on top and the breaking asset replaced

--- app/components/Homepage/testimonials.tsx
+++ app/components/Homepage/testimonials.tsx
@@ -42,6 +42,7 @@ const Testimonials = () => {
   }
   return (
     <div id="testimonials" aria-label='testimonials' role="complementary" className="section-container section-p-y">
+      <span className="anchor" id="testimonials"></span>
       <section className="section-heading">
         <p
           role="heading"
@@ -130,7 +131,7 @@ const Testimonials = () => {
                     <div className="flex text-sm py-4 font-poppins font-normal subtitle justify-between">
                       <div className="flex text-start">{testimonial.subtitle}</div>
                       <div className="flex mb-4">
-                        <img src="../assets/Quote.svg" alt="vector" />
+                        <svg aria-hidden="true" role="presentation" width="44" height="33" viewBox="0 0 44 33" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#a)"><path d="M0 0v16.5h11c0 6.064-4.93 11-11 11V33c9.098 0 16.5-7.402 16.5-16.5V0zm27.5 0v16.5h11c0 6.064-4.93 11-11 11V33C36.598 33 44 25.598 44 16.5V0z" fill="#fff" fill-opacity=".25"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h44v33H0z"/></clipPath></defs></svg>
                       </div>
                     </div>
                     <div
